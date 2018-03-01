5a2073ced5bef3723c9ff8b6c73403712acb4485
Harry Waugh <hw16471@my.bristol.ac.uk>
2018-03-01 15:27:50 +0000

    Removed repeated code

--- kernel/lolevel.s
+++ kernel/lolevel.s
@@ -14,22 +14,6 @@
 .global lolevel_handler_irq
 .global lolevel_handler_svc
 
-/* Copyright (C) 2017 Daniel Page <csdsp@bristol.ac.uk>
- *
- * Use of this source code is restricted per the CC BY-NC-ND license, a copy of
- * which can be found via http://creativecommons.org (and should be included as
- * LICENSE.txt within the associated archive or repository).
- */
-
-/* Each of the following is a low-level interrupt handler: each one is
- * tasked with handling a different interrupt type, and acts as a sort
- * of wrapper around a high-level, C-based handler.
- */
-
-.global lolevel_handler_rst
-.global lolevel_handler_svc
-.global lolevel_handler_irq
-
 lolevel_handler_rst: bl    int_init                @ initialise interrupt vector table
 
                      msr   cpsr, #0xD3             @ enter SVC mode with IRQ and FIQ interrupts disabled
